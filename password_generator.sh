8662e56a5c4c831cc3234cb2ea500ef702040a41
1993Lukas1993 <lukasbubeliny@gmail.com>
2023-03-27 12:02:25 +0000

    added password generator script

---
+++ password_generator.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Simple Password Generator
+
+echo ""
+echo "===THIS IS A SIMPLE PASSWORD GENERATOR==="
+echo ""
+
+read -p "Please enter the lenght of the password (min 6/max 36): " PASS_LENGHT
+
+if [ $PASS_LENGHT -gt 36 ]; then
+        echo "Please enter range from 6 to 36"
+        exit
+elif [ $PASS_LENGHT -lt 6 ]; then
+        echo "Please enter range from 6 to 36"
+        exit
+fi
+
+echo ""
+read -p "Please enter the number of passwords, which you want to generate: " PASS_COUNT
+echo ""
+echo "creating $PASS_COUNT password/s with lenght of $PASS_LENGHT"
+echo ""
+echo "========Here are your passwords========"
+
+for i in $( seq 1 $PASS_COUNT );do
+	openssl rand -base64 $PASS_LENGHT
+done
+
