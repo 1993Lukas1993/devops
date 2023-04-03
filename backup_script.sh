d9ae833dfb8510ac21ba636d529e988620a86896
1993Lukas1993 <125473449+1993Lukas1993@users.noreply.github.com>
2023-04-03 09:17:54 +0200

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -8,19 +8,19 @@ LOGFILE=backup_log.txt
 
 # testing of arguments
 
-	if [ $# != 2 ]; then
-		echo "Usage: ./backup.sh <SOURCE> <TARGET>"
-		exit 1
-	fi
+        if [ $# != 2 ]; then
+                echo "Usage: ./backup.sh <SOURCE> <TARGET>"
+                exit 1
+        fi
 
 # List of functions
 
 check_source () {
 
-	if [ ! -d $BACKUP_SOURCE ]; then
-		echo "Directory which you want to backup does not exist, please choose other directory."
-		exit 1
-	fi
+        if [ ! -d $BACKUP_SOURCE ]; then
+                echo "Directory which you want to backup does not exist, please choose other directory."
+                exit 1
+        fi
 
 }
 
@@ -28,30 +28,31 @@ check_source () {
 check_target () {
 
 
-	if [ -d $BACKUP_TARGET ]; then
-	echo "Directory $BACKUP_TARGET already exist, Do you want to overwrite it?"
-	echo "===================================================="
-	echo "1 - yes, overwrite $BACKUP_TARGET"
-	echo "2 - no, exit the script"
-	echo "===================================================="
+        if [ -d $BACKUP_TARGET ]; then
+        echo "Directory $BACKUP_TARGET already exist, Do you want to overwrite it?"
+        echo "===================================================="
+        echo "1 - yes, overwrite $BACKUP_TARGET"
+        echo "2 - no, exit the script"
+        echo "===================================================="
 
-	read -s OVERWRITE
+        read  OVERWRITE
 
-	case $OVERWRITE in 
-	
-	1) rm -rf $BACKUP_TARGET && mkdir $BACKUP_TARGET && echo "Target directory was overwritted" ;;
-	2) echo "Exiting script" && exit 1 ;;
-	*) echo "Please select yes or no" && exit 1 ;;
-		
-	esac 
-		
-	else
-		
-	mkdir $BACKUP_TARGET
-	return 0
+        case $OVERWRITE in
 
-	fi
+        1) rm -rf $BACKUP_TARGET && mkdir $BACKUP_TARGET && echo "Target directory was overwritted" ;;
+        2) echo "Exiting script" && exit 1 ;;
+        *) echo "Please select yes or no" && exit 1 ;;
+
+        esac
+
+        else
+
+        mkdir $BACKUP_TARGET
+        return 0
+
+        fi
 }
+
 
 # Calling of functions
 
@@ -61,23 +62,23 @@ check_target
 
 # Testing for backup log
 
-	if [ -f $LOGFILE ]; then
-		rm -rf $LOGFILE
-	fi
+        if [ -f $LOGFILE ]; then
+                rm -rf $LOGFILE
+        fi
 
 # Copying of files
 
-copy () {	
-	echo "Copying files:" 
-	cp -v $BACKUP_SOURCE/* $BACKUP_TARGET 
-	echo "====================================================" 
-	echo "Files copied:" 
-	echo "$( ls -latrh $BACKUP_TARGET )"
-	echo "====================================================" 
-	echo "This backup was performed at `date`"
-} 
+copy () {
+        echo "Copying files:"
+        cp -Rv $BACKUP_SOURCE/* $BACKUP_TARGET
+        echo "===================================================="
+        echo "Files copied:"
+        echo "$( ls -latrh $BACKUP_TARGET )"
+        echo "===================================================="
+        echo "This backup was performed at `date`"
+}
 
 copy 2>&1 > $LOGFILE
 
-
-
+clear
+cat $LOGFILE
